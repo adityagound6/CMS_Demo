88fc4634c0e5c7f731702eda3192e582010c72ee
Yugal <yugalbxn@gmail.com>
2022-03-31 12:45:50 +0530

    q

--- Startup.cs
+++ Startup.cs
@@ -22,8 +22,8 @@ namespace CMS_Demo
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContextPool<AppDbContext>(
-                  options => options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
+           /* services.AddDbContextPool<AppDbContext>(
+                  options => options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
