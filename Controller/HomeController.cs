694e31b9df971051daa987cf242b150144b5460a
Yugal <yugalbxn@gmail.com>
2022-03-31 14:12:26 +0530

    AppDbContext

---
+++ Controller/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using CMS_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS_Demo.Controller
+{
+    public class HomeController
+    {
+        private readonly AppDbContext _con;
+
+        public HomeController(AppDbContext con)
+        {
+            _con = con;
+        }
+    }
+}
---
+++ Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS_Demo.Models
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<DbContext> options)  :base(options)
+        {
+
+        }
+        public DbSet<Users> users { get; set; }
+    }
+}
