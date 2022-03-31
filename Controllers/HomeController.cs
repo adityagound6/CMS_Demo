531e15eaa0bcf823b611e10d33354e95964b12be
Yugal <yugalbxn@gmail.com>
2022-03-31 14:20:51 +0530

    nne

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,18 +1,24 @@
 ﻿using CMS_Demo.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CMS_Demo.Controller
+namespace CMS_Demo.Controllers
 {
-    public class HomeController
+    public class HomeController:Controller
     {
         private readonly AppDbContext _con;
 
         public HomeController(AppDbContext con)
         {
             _con = con;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
         }
     }
 }
