eebcd0d0f3e00a32f71c1e7c21a523c4b21a9b0b
Aditya <adityagound6@gmail.com>
2022-03-31 12:53:55 +0530

    user_model

---
+++ Models/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS_Demo.Models
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+    }
+}
