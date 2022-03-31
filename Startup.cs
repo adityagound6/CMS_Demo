04be6a6986bebb13159e4c1035b8e5e715ddc9a6
Yugal <yugalbxn@gmail.com>
2022-03-31 15:02:09 +0530

    Login

--- Startup.cs
+++ Startup.cs
@@ -21,16 +21,13 @@ namespace CMS_Demo
             this._config = _config;
         }
         public IConfiguration Configuration { get; }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContextPool<AppDbContext>(option =>
-            option.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
+                        option.UseSqlServer(_config.GetConnectionString("EmployeeDBConnection")));
 
             services.AddMvc(options => options.EnableEndpointRouting = false);
-            /* services.AddDbContextPool<AppDbContext>(
-                   options => options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function getResponce(deptId, empId) {
+    if (confirm("Are You sure To delete This User?") == true) {
+        window.location.replace(`/admin/delete?deptId=${deptId}&empId=${empId}`)
+    }
+}
