ee3e14f494cf2063911a7d5f87d8052d97d5acef
Aditya <adityagound6@gmail.com>
2022-04-14 17:32:35 +0530

    wrong

---
+++ Migrations/20220414115828_footer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CMS_Demo.Migrations
+{
+    public partial class footer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
